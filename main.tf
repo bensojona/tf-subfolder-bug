a0cf0e2f7885bfb64a354a0211694b85c651be3d
Jon Benson <bensojon@gmail.com>
2015-10-02 13:05:46 -0700

    Add test files

---
+++ main.tf
@@ -0,0 +1,43 @@
+variable "name" { default = "main-folder-test" }
+variable "region" { default = "us-east-1" }
+variable "atlas_environment" { default = "main-folder-test" }
+variable "atlas_username" { default = "jb_hashicorp" }
+variable "cidr" { default = "10.139.0.0/16" }
+
+provider "aws" {
+  region = "${var.region}"
+}
+
+atlas {
+  name = "${var.atlas_username}/${var.atlas_environment}"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "${var.cidr}"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags { Name = "${var.name}" }
+}
+
+resource "aws_security_group" "consul" {
+  name        = "${var.name}"
+  vpc_id      = "${aws_vpc.vpc.id}"
+  description = "Security group for Consul"
+
+  tags { Name = "${var.name}" }
+
+  ingress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
